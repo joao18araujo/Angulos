a2f11eb5401b28a071b98334d2429372f89a8fe5
joao18araujo <joao18araujo@gmail.com>
2014-09-22 22:08:14 -0300

    Update angulo.c

--- angulo.c
+++ angulo.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
+double grausrad(angulograus)
+{
+	double angulogras, angulorad;
+	angulorad=angulograus*3.1415926536/180;
+	return angulorad;
+}
+
 int main()
 {
-  double angulo;
-  printf("Digite o angulo: ");
-  scanf("%lf", &angulo);
-  printf("sen = %f, cos = %f\n", sin(angulo), cos(angulo));
-  printf("tg = %f\n", tan(angulo));
-  return 0;
+	double angulo;
+	printf("Digite o angulo (em graus): ");
+	scanf("%lf", &angulo);
+	printf("%f\n", grausrad(angulo));
+	printf("sen = %f, cos = %f\n", sin(angulo), cos(angulo));
+	printf("tg = %f\n", tan(angulo));
+	return 0;
 }
